b20a52a2880d4e603e436aaa6f9a32040102af13
Kate <KittyKate@Elliots-MBP.fritz.box>
2024-03-27 18:21:25 +1300

    finished, debugged

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -1,22 +1,22 @@
-const testArray = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
+const testArray = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 const fizzBuzz = (arr) => {
   for (let i = 0; i <= arr.length; i++) {
-    const num = arr[i];
+    const num = arr[i]
 
     if (num % 15 === 0) {
-      console.log("FizzBuzz");
+      console.log('FizzBuzz')
     } else if (num % 3 === 0) {
-      console.log("Fizz");
+      console.log('Fizz')
     } else if (num % 5 === 0) {
-      console.log("Buzz");
-    } else if (i === arr.length-1) 
-    {break} else {
-      console.log(num);
+      console.log('Buzz')
+    } else {
+      console.log(num)
     }
-
-    
+    if (i === arr.length - 1) {
+      break
+    }
   }
 }
 
-fizzBuzz(testArray);
+fizzBuzz(testArray)
